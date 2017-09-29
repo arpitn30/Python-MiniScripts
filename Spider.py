b3860b9fadcbc36c31483d3ad6e86b3834145262
Arpit Nandwani <arpit.nandwani@gmail.com>
2017-09-29 11:42:32 +0530

    Updated Spider

--- Spider.py
+++ Spider.py
@@ -1,70 +1,51 @@
-from html.parser import HTMLParser
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from urllib import parse
-# from sys import argv
 
 
-class LinkParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        if tag == 'a':
-            for (key, value) in attrs:
-                if key == 'href':
-                    # Get all the links on the page
-                    newUrl = parse.urljoin(self.baseUrl, value)
-                    # Add only the new links to self.links
-                    if newUrl not in self.links:
-                        self.links = self.links + [newUrl]
+def main():
+    url = input('Enter the URL of the website to search: ')
+    word = input('Enter the word(s) to search: ')
+    maxPages = int(input('Enter the max no of pages to search: '))
 
-    def getLinks(self, url):
-        self.links = []
-        self.baseUrl = url
-        response = urlopen(url)
-        # Only access if the file is html
-        if response.getheader('Content-Type') == 'text/html':
-            # Read the html data on the page
-            htmlBytes = response.read()
-            htmlString = htmlBytes.decode('utf-8')
-            # feed only reads strings and not bytes
-            self.feed(htmlString)
-            # Called handle_starttag and get links on the page
-            return htmlString, self.links
-        else:
-            return '', []
-
-
-def spider(url, word, maxPages):
-    pagesToVisit = [url]
-    numberVisited = 0
-    i = 0
+    Links = [url]
+    numberVisited, i = 0, 0
     foundWord = []
-    while numberVisited < maxPages and pagesToVisit != []:
+
+    # Run loop until the max number of pages are visited or no more links are there to visit
+    while numberVisited < maxPages and i < len(Links):
         numberVisited = numberVisited + 1
-        url = pagesToVisit[i]
+        # Get the ith url in the Links list
+        url = Links[i]
         i = i + 1
-        # pagesToVisit = pagesToVisit[1:]
         try:
             print(numberVisited, 'Visiting: ', url)
-            parser = LinkParser()
-            data, links = parser.getLinks(url)
-            # Find the word in the html data of the page
-            if data.find(word) > -1:
+            # Parse the html in the given url
+            soup = BeautifulSoup(urlopen(url), 'html.parser')
+
+            # Get the text in head and body
+            head = soup.find('head')
+            text = head.get_text()
+            body = soup.find('body')
+            text = text + '\n' + body.get_text()
+            # Find the word in the text and if found, store the url
+            if (text.find(word) != -1) and (url not in foundWord):
                 foundWord.append(url)
-            # Add the links in the Links List to pagesToVisit after removing duplicates
-            pagesToVisit.extend(page for page in links if page not in pagesToVisit)
+
+            # Add all the urls on the page to the Links list
+            Links.extend(page['href'] for page in soup.find_all('a', href=True)
+                        if page['href'] not in Links
+                        and page['href'].startswith(Links[0]))
+
             print('**Success!**')
         except:
             print('**Failed!**')
 
-    if foundWord is not []:
-        print('The word', word, 'was found at:')
+    if foundWord != []:
+        print('\nThe word', word, 'was found at:')
         print(*foundWord, sep='\n')
     else:
-        print('Word never found')
+        print('\nWord never found')
 
 
 if __name__ == '__main__':
-    url = input('Enter the URL of the website to search: ')
-    word = input('Enter the word(s) to search: ')
-    maxPages = input('Enter the max no of pages to search: ')
-    #script, url, word, maxPages = argv
-    spider(url, word, int(maxPages))
+    main()
--- TOSbot.py
+++
@@ -1,5 +0,0 @@
-import win32com.client as comclt
-wsh = comclt.Dispatch("WScript.Shell")
-wsh.AppActivate("Client_tos")
-for i in range(100000)
-    wsh.SendKeys("z")
